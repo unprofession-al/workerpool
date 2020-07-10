120c170160bcaa739322abb3c30c5ad7fd69b310
Daniel Menet <membership@sontags.ch>
2020-07-10 08:23:46 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/unprofession-al/workerpool
+
+go 1.14
+
+require (
+	github.com/gosuri/uilive v0.0.4 // indirect
+	github.com/gosuri/uiprogress v0.0.1
+)
---
+++ workerpool.go
@@ -0,0 +1,74 @@
+package workerpool
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/gosuri/uiprogress"
+)
+
+type Pool struct {
+	jobs []func()
+
+	concurrency int
+	jobChan     chan func()
+	wg          sync.WaitGroup
+
+	bar *uiprogress.Bar
+}
+
+type Job interface {
+	Run(*sync.WaitGroup)
+}
+
+func NewPool(concurrency int) *Pool {
+	return &Pool{
+		concurrency: concurrency,
+		jobChan:     make(chan func()),
+	}
+}
+
+func (p *Pool) Add(job func()) {
+	p.jobs = append(p.jobs, job)
+}
+
+func (p *Pool) Run(progress bool) {
+	if progress {
+		uiprogress.Start()
+		p.bar = uiprogress.AddBar(len(p.jobs))
+		p.bar.AppendCompleted()
+		p.bar.PrependElapsed()
+		p.bar.PrependFunc(func(b *uiprogress.Bar) string {
+			return fmt.Sprintf("Task (%d/%d)", b.Current(), len(p.jobs))
+		})
+	}
+
+	for i := 0; i < p.concurrency; i++ {
+		go func() {
+			p.work(i)
+		}()
+	}
+
+	p.wg.Add(len(p.jobs))
+	for _, job := range p.jobs {
+		p.jobChan <- job
+	}
+
+	close(p.jobChan)
+
+	p.wg.Wait()
+
+	if progress {
+		uiprogress.Stop()
+	}
+}
+
+func (p *Pool) work(i int) {
+	for job := range p.jobChan {
+		job()
+		p.wg.Done()
+		if p.bar != nil {
+			p.bar.Incr()
+		}
+	}
+}
